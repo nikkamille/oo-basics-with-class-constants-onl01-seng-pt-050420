e3c35d8c33fba8685c9cbe1209e3ca66022508aa
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-21 01:34:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+  end
+  
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand
   end
 
   def cobble
